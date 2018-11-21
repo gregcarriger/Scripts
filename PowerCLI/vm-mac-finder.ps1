57ff599dd4060e8353edd2ef75ae546436b97ce7
Greg Carriger <gregcarriger@users.noreply.github.com>
2018-11-21 13:02:56 -0600

    old stuff doesn't work anymore.

--- PowerCLI/vm-mac-finder.ps1
+++ PowerCLI/vm-mac-finder.ps1
@@ -1,2 +1,2 @@
 # MAC finder
-get-vm | Get-NetworkAdapter | Select-String 00:50:56:00:11:22:33:44
+get-vm | Get-NetworkAdapter | Where-Object MacAddress -eq "00:50:56:00:00:00" | Select-Object Parent,Name,MacAddress

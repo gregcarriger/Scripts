836361a1696ceb8fcd5c8cc38f5cf9e2724851d5
Greg Carriger <gregcarriger@users.noreply.github.com>
2021-08-18 10:10:26 -0500

    Update compare-files.ps1

--- PowerCLI/compare-files.ps1
+++ PowerCLI/compare-files.ps1
@@ -1,8 +1,19 @@
-# Compare transfered files
+# Variables
 $file = "D:\database.bak"
 $remotesystem = server.company.com
+$localdrive = g
+$useraccount = company\username
+$bucket = exmaple-bucket
+$pass = "hunter2"
+$awsregion = us-gov-west-1
+$accesskey = ABCDEFGHIJKLMNOPQRST
+$accesskeysecret = ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
 
 # Script
+# Move file
+net use $localdrive":" "\\"$remotesystem"\"$localdrive"`$ $pass /user:$useraccount /p:yes
+
+# Check file
 $filehash = (Get-FileHash $file).hash
 $filelength = (Get-item $file).Length
 Invoke-Command -ComputerName $remotesystem -ScriptBlock {
@@ -12,4 +23,10 @@ Invoke-Command -ComputerName $remotesystem -ScriptBlock {
 	:filehash -eq $remotefilehash
 	Write-Output "Do the file lengths match?"
 	:filelength -eq $remotefilelength
+}
+
+# Move file to s3
+Invoke-Command -ComputerName $remotesystem -ScriptBlock {
+	aws configure set region $region --aws_access_key_id $accesskey --aws_secret_access_key $accesskeysecret
+	aws s3 cp $localdrive":\"$file s3://$bucket/$file
 }

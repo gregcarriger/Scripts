44d4bba8792c2839ba5ad8ece35a554bc8b847e1
Greg Carriger <gregcarriger@users.noreply.github.com>
2021-08-18 09:59:51 -0500

    Update compare-files.ps1

--- PowerCLI/compare-files.ps1
+++ PowerCLI/compare-files.ps1
@@ -3,11 +3,11 @@ $file = "D:\database.bak"
 $remotesystem = server.company.com
 
 # Script
-$filehash = Get-FileHash $file
-$filelength = Get-item $file | Select-Object Length
+$filehash = (Get-FileHash $file).hash
+$filelength = (Get-item $file).Length
 Invoke-Command -ComputerName $remotesystem -ScriptBlock {
-	$remotefilehash = Get-FileHash $file
-	$remotefilelength = Get-item $file | Select-Object Length
+	$remotefilehash = (Get-FileHash $file).hash
+	$remotefilelength = (Get-item $file).Length
 	Write-Output "Do the file hashes match?"
 	:filehash -eq $remotefilehash
 	Write-Output "Do the file lengths match?"

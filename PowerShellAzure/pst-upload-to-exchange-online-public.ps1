2e2ab58425c86cb9dfe6c54740de90357cae7147
Greg Carriger <gregcarriger@users.noreply.github.com>
2021-10-08 14:13:12 -0500

    Update pst-upload-to-exchange-online-public.ps1
    tested

--- PowerShellAzure/pst-upload-to-exchange-online-public.ps1
+++ PowerShellAzure/pst-upload-to-exchange-online-public.ps1
@@ -25,7 +25,7 @@
 ########## Prod variables
 $source_pst_location = "\\company\shared\drive\PST-folder\"
 $azcopy_log_directory = "C:\temp\"
-$blob_service_sas_URL = "https://abcdefghijklmnopqrstuvwxyz.blob.ingestion.blobl4.protection.office365.us/ingestiondata?sv=2015-04-05&sr=c&si=IngestionSasForAzCopy1234567890123456789&sig=12345678901234567890123456789&se=2021-11-04T18"
+$blob_service_sas_URL = "https://abcdefghijklmnopqrstuvwxyz.blob.ingestion.blobl4.protection.office365.com/ingestiondata?sv=2015-04-05&sr=c&si=IngestionSasForAzCopy1234567890123456789&sig=12345678901234567890123456789&se=2021-11-04T18"
 $azcopy_exe = "C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe"
 
 # Fix variable
@@ -34,18 +34,10 @@ $source_pst_location_fixed = $source_pst_location.replace('\','\\')
 # Test if ready to start
 if ($(test-path $source_pst_location) -EQ "True" -And $(test-path $azcopy_log_directory) -EQ "True" -And $(test-path $azcopy_exe) -EQ "True")
 {
-write-output "Does source_pst_location exist?"
-test-path $source_pst_location
-write-output "Does azcopy_log_directory exist?"
-test-path $azcopy_log_directory
-write-output "Does azcopy_exe exist?"
-test-path $azcopy_exe
-write-output "Use CTRL+C to stop the script and make sure files and folders exist."
-pause
-}
+
 
 # Get unprocessed PST files and data for the import CSV
-$pst_files = Get-ChildItem -path $source_pst_location -Recurse -Filter "*.pst" | Select-Object name,fullname,length#,LastAccessTimeUtc
+$pst_files = Get-ChildItem -path $source_pst_location -Recurse -Filter "*.pst" | Select-Object name,fullname,length
 $pst_files | Add-Member -NotePropertyName TargetRootFolder -NotePropertyValue $null
 $pst_files | Add-Member -NotePropertyName Status -NotePropertyValue $null
 $pst_files | Add-Member -NotePropertyName NewFullName -NotePropertyValue $null
@@ -61,21 +53,23 @@ $pst_files | ForEach-Object {
 	$_.{TargetRootFolder} = $_.{FullName} -replace $source_pst_location_fixed , ""
 	$_.{TargetRootFolder} = $_.{TargetRootFolder}.replace('\','-')
 	$_.{TargetRootFolder} = $_.{TargetRootFolder}
-	$_.{Name} = $_.{TargetRootFolder}
 	$_.{NewFullName} = $_.{FullName} -replace $_.{Name} , $_.{TargetRootFolder}
 	$_.Mailbox = (get-aduser -Identity (get-item $_.FullName).directory.name).UserPrincipalName
+	$_.{Name} = $_.{TargetRootFolder}
 
 	# rename the big pst files
 	If ($_.Length -gt 19000000000)
 		{
 		Rename-Item -Path $_.FullName -NewName "$($_.Name).toobig"
 		$_.Status = "Renamed .big"
+		write-output "$($_.Name) renamed to $($_.Name).toobig"
 	}
 	Elseif ($_.{FullName} -replace $source_pst_location_fixed , "" )
 		{
 		# add folder name to pst files
 		Rename-Item -Path $_.FullName -NewName $_.TargetRootFolder
 		$_.Status = "Appended directory name to filename"
+		write-output "$($_.Name) renamed to $($_.TargetRootFolder)"
 	}
 	Else
 		{
@@ -83,11 +77,25 @@ $pst_files | ForEach-Object {
 }
 
 # generate .csv needed for exchange online import.
-$pst_files | Select-Object Workload,FilePath,Name,Mailbox,IsArchive,TargetRootFolder,ContentCodePage,SPFileContainer,SPManifestContainer,SPSiteUrl | export-csv -NoTypeInformation $($azcopy_log_directory)PstImportMappingFile.csv
+$pst_files | Select-Object Workload,FilePath,Name,Mailbox,IsArchive,TargetRootFolder,ContentCodePage,SPFileContainer,SPManifestContainer,SPSiteUrl | export-csv -NoTypeInformation "$($azcopy_log_directory)PstImportMappingFile.csv"
 write-output "Please use $($azcopy_log_directory)PstImportMappingFile.csv for the Exchange Online pst import."
 
 # upload .pst files
 $pst_files | ForEach-Object {
-	Invoke-Item $azcopy_exe "/Source:$($_.Name) /Dest:$($blob_service_sas_URL) /Pattern:$($_.name) /v:$(($azcopy_log_directory)+$($_.Name)+"-azcopy.log") /y"
-	Rename-Item -Path $_.FullName -NewName "$($_.Name).uploaded"
+	& $azcopy_exe  /Source:$([string]($_.NewFullName).Substring(0, $_.NewFullName.lastIndexOf('\'))) /Dest:$($blob_service_sas_URL) /Pattern:$($_.name) /v:$($azcopy_log_directory)$($_.Name)-azcopy.log /y
+#	Rename-Item -Path $_.NewFullName -NewName "$($_.Name).uploaded
+}
+$pst_files | export-csv -NoTypeInformation $azcopy_log_directory"Pst_files_debug.csv"
+
+}
+else
+{
+write-output "Does source_pst_location exist?"
+test-path $source_pst_location
+write-output "Does azcopy_log_directory exist?"
+test-path $azcopy_log_directory
+write-output "Does azcopy_exe exist?"
+test-path $azcopy_exe
+write-output "Use CTRL+C to stop the script and make sure files and folders exist."
+pause
 }
